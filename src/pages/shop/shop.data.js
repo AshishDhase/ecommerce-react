c19337a4a80e14a29a47694f9416d660c86a1eb3
unknown <ashishdhase59@gmail.com>
2022-01-20 19:35:08 +0530

    Updating app with shop page,collection preview component and collection item component

---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,99 @@
+const SHOP_DATA = [
+    {
+        id: 1,
+        title: 'Hats',
+        routeName: 'hats',
+        items : [
+            {
+                id: 1,
+                name: 'Brown Brim',
+                imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
+                price: 25
+            },
+            {
+                id: 2,
+                name: 'Blue Beanie',
+                imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+                price: 18
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: 'Sneakers',
+        routeName: 'sneakers',
+        items : [
+            {
+                id: 1,
+                name: 'Adidas NMD',
+                imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
+                price: 220
+            },
+            {
+                id: 2,
+                name: 'Adidas Yeezy',
+                imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
+                price: 18
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: 'Jackets',
+        routeName: 'jackets',
+        items : [
+            {
+                id: 1,
+                name: 'Black Jean Shearling',
+                imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
+                price: 125
+            },
+            {
+                id: 2,
+                name: 'Blue Jean Jacket',
+                imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
+                price: 90
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: 'Womens',
+        routeName: 'womens',
+        items : [
+            {
+                id: 1,
+                name: 'Blue Tanktop',
+                imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
+                price: 25
+            },
+            {
+                id: 2,
+                name: 'Floral Blouse',
+                imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
+                price: 20
+            }
+        ]
+    },
+    {
+        id: 5,
+        title: 'Mens',
+        routeName: 'mens',
+        items : [
+            {
+                id: 1,
+                name: 'Camo Down Vest',
+                imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
+                price: 325
+            },
+            {
+                id: 2,
+                name: 'Floral T-shirt',
+                imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
+                price: 20
+            }
+        ]
+    }
+]
+
+export default SHOP_DATA;
