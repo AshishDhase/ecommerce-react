9b40a7a92aa7a0e886327dcce31973cec253cc20
unknown <ashishdhase59@gmail.com>
2022-01-26 23:07:03 +0530

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -24,13 +24,13 @@ const SHOP_DATA = [
         routeName: 'sneakers',
         items : [
             {
-                id: 1,
+                id: 3,
                 name: 'Adidas NMD',
                 imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
                 price: 220
             },
             {
-                id: 2,
+                id: 4,
                 name: 'Adidas Yeezy',
                 imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
                 price: 18
@@ -43,13 +43,13 @@ const SHOP_DATA = [
         routeName: 'jackets',
         items : [
             {
-                id: 1,
+                id: 5,
                 name: 'Black Jean Shearling',
                 imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
                 price: 125
             },
             {
-                id: 2,
+                id: 6,
                 name: 'Blue Jean Jacket',
                 imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
                 price: 90
@@ -62,13 +62,13 @@ const SHOP_DATA = [
         routeName: 'womens',
         items : [
             {
-                id: 1,
+                id: 7,
                 name: 'Blue Tanktop',
                 imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
                 price: 25
             },
             {
-                id: 2,
+                id: 8,
                 name: 'Floral Blouse',
                 imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
                 price: 20
@@ -81,13 +81,13 @@ const SHOP_DATA = [
         routeName: 'mens',
         items : [
             {
-                id: 1,
+                id: 9,
                 name: 'Camo Down Vest',
                 imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
                 price: 325
             },
             {
-                id: 2,
+                id: 10,
                 name: 'Floral T-shirt',
                 imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
                 price: 20
